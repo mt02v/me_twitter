2985df1cfc35757ead40f62eca0a2075d9a73edb
mt02v <141305346+mt02v@users.noreply.github.com>
2024-03-29 14:59:55 +0000

    [Finish] 初めてのRuby on Rails。Twitterみたいなサイトを作成しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    if params[:username] == 'tum'
-      @user[:name] = 'tumutumu'
-      @user[:username] = 'tum'
-      @user[:location] = 'Tokyo, Japan'
-      @user[:about] = 'Hello'
-    elsif params[:username] == 'mu'
-      @user[:name] = 'mumu'
-      @user[:username] = 'mu'
-      @user[:location] = 'Yamaguchi, Japan'
-      @user[:about] = 'I’m mumu'
-    end
+    @user  = User.find_by(:username => params[:username])
   end
 end
